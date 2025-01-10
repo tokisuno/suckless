4adb92f2621319018f13ac59aea1529077a71ea5
tokisuno <lucaswmei@gmail.com>
2025-01-09 20:27:50 -0500

    dwm changes

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                        /*Update Interval*/	    /*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"MINS:", "~/.local/share/scripts/minutes_left",	60,		0},
 };
 
