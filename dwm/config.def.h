141429a6e63baee198deb229bfc636333d68bae5
tokisuno <lucaswmei@gmail.com>
2025-01-28 09:50:59 -0500

    config changes

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -17,7 +17,7 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#504945";
 static const char col_gray3[]       = "#d5c4a1";
 static const char col_gray4[]       = "#fbf1c7";
-static const char col_cyan[]        = "#fabd2f";
+static const char col_cyan[]        = "#b57614";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -31,9 +31,9 @@ static const char *const autostart[] = {
     "pactl", "set-default-sink", "alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo", NULL,
     "pactl", "set-sink-volume", "@DEFAULT_SINK@ 25%", NULL,
     "syncthing", NULL,
-    "dwmblocks", NULL,
     "setxkbmap", "-option", "'ctrl:nocaps'", NULL,
     "picom", NULL,
+    "sh", "-c", "$HOME/dwm_config/bar", NULL,
     "sh", "-c", "feh --bg-fill $HOME/wallpapers/gruv-temple.png", NULL,
     NULL
 };
@@ -77,7 +77,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -130,7 +129,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	/*{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },*/
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
--- readme.md
+++ readme.md
@@ -1,14 +1,18 @@
-# tokisuno suckless configurations
+# tokisuno dwm config
 I was programming in Ruby and was getting really annoyed with i3's window splitting and movements. So I spent the entire day learning dwm instead!
+
+The goal of this is to just configure it, and then leave it for a long period of time. Although the commit history doesn't look like it, I don't really like to change my config that much at the moment, and the frustrations of dwm make that possible!!
 
 # includes
 - dwm
 - dwmblocks
-- st
 
 # patches
 ## dwm
-- autostart
+- cool-autostart
 - fullgaps
 - systray
 - alwayscenter
+
+## dwmblocks
+- systray
